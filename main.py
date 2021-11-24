6b053d7eadec35b38c3ebf10e6dfacac1857cf0d
Timur.Sharafutdinov <Timur.Sharafutdinov@south.rt.ru>
2021-11-24 13:04:31 +0300

    finished homework #2

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randint
+
 """
 Решение задач по курсу
 """
@@ -54,3 +56,31 @@ def find_roots(a, b, c):
         return f'Найдена единственный корень {x}'
     elif disc < 0:
         return f'Корней нет'
+
+
+# 7
+def find_euclid_dist(a, b):
+
+    return print(((a[0] - b[0])**2 + (a[1] - b[1])**2)**0.5)
+
+
+# 9
+rand_nums = [randint(10, 40) for i in range(50)]
+filtered_nums = [n for n in rand_nums if n % 2 == 0 and n % 5 ==0]
+
+
+
+# 10
+rand_dict = {k: [randint(0, 100) for i in range(10)] for k in [x for x in range(5)]}
+sums = [sum(v) for v in list(rand_dict.values())]
+
+
+#11
+given_list_of_nums = [4, 78, 99, 872]
+try_count = 0
+while len(given_list_of_nums):
+    rng_num = randint(0, 10000)
+    if rng_num in given_list_of_nums:
+        given_list_of_nums.remove(rng_num)
+    try_count += 1
+print(try_count)
